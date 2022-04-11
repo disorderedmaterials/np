5315f96da0799e364db5f14399e8c6b31a6d89c6
Jared Swift <jared.swift@stfc.ac.uk>
2022-04-11 10:53:19 +0100

    feat: update main.

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 int main(void) {
 
     std::vector<std::string> runs = {"data/NIMROD00069862.nxs"};
-    ModEx m("azobenzene.txt", "testout", runs);
+    ModEx m("azobenzene.txt", "purge_det.dat", "testout", runs);
     std::map<std::string, std::vector<std::pair<double, double>>> pulses;
     m.extrapolatePulseTimes(
         runs[0], 12129., false, true, 980., 300., pulses
