d0bc40cbe26318bb507504f0e3a5df4d3b4a67d1
Jared Swift <jared.swift@stfc.ac.uk>
2022-04-11 10:53:08 +0100

    doc/fix: code commenting and deleting heap objects.

--- src/modex.cpp
+++ src/modex.cpp
@@ -12,20 +12,31 @@ bool ModEx::run() {
 
 bool ModEx::run(std::map<std::string, std::vector<std::pair<double, double>>> &runPulses) {
 
+    // Perform a purge.
+    system(std::string("purge " + purge).c_str());
+    // Iterate through [run, pulse] pairs.
     for (const auto runPair : runPulses) {
         std::string run = runPair.first;
         std::vector<std::pair<double, double>> pulses = runPair.second;
 
+        // Iterate through pulses.
         for (auto p : pulses) {
+            // Output path = test/{runName}.nxs
             std::string nxsPath = "test/" + run.substr(run.find_last_of("/\\") + 1);
+            // Allocate a new Nexus objecs on the heap.
             Nexus *nxs = new Nexus(run, nxsPath);
+            // Load event mode data.
             nxs->loadEventModeData();
+            // Create histogram using the pulse as boundaries.
             nxs->createHistogram(p);
+            // Write the histogram.
             nxs->writeCountsHistogram();
-            system("purge_det purge_det.dat");
+            // Call gudrun_dcs.
             system(std::string("gudrun_dcs " + input).c_str());
+            // Move the mint01 file to the output directory.
             std::string output = std::string(out + "/" + std::to_string(p.first) + ".mint01");
             rename(nxsPath.c_str(), output.c_str());
+            delete nxs;
         }
     }
 }
@@ -38,9 +49,11 @@ bool ModEx::extrapolatePulseTimes(std::string start_run, double start, bool back
 
     std::vector<std::pair<double, double>> pulses;
 
+    // Assume runs are ordered.
     const std::string firstRun = runs[0];
     const std::string lastRun = runs[runs.size()-1];
 
+    // Load the first, last and start runs.
     Nexus firstRunNXS(firstRun);
     firstRunNXS.loadBasicData();
     Nexus lastRunNXS(lastRun);
@@ -48,12 +61,14 @@ bool ModEx::extrapolatePulseTimes(std::string start_run, double start, bool back
     Nexus startRunNXS(start_run);
     startRunNXS.loadBasicData();
 
+    // Determine start, end and first pulse times, since unix epoch.
     const int expStart = firstRunNXS.startSinceEpoch;
     const int expEnd = lastRunNXS.endSinceEpoch;
     double startPulse = startRunNXS.startSinceEpoch + start;
     double pulse = 0;
     pulses.push_back(std::make_pair(startPulse, startPulse+step));
 
+    // Extrapolate backwardsa.
     if (backwards) {
         pulse = startPulse - step;
         std::cout << startPulse << " " << step << std::endl;
@@ -63,6 +78,7 @@ bool ModEx::extrapolatePulseTimes(std::string start_run, double start, bool back
         }
     }
 
+    // Extrapolate forwards.
     if (forwards) {
         pulse = startPulse + step;
         while (pulse < expEnd) {
@@ -70,6 +86,8 @@ bool ModEx::extrapolatePulseTimes(std::string start_run, double start, bool back
             pulse+=step;
         }
     }
+
+    // Sort pulses by their start time.
     std::sort(
         pulses.begin(), pulses.end(),
         [](
@@ -79,15 +97,20 @@ bool ModEx::extrapolatePulseTimes(std::string start_run, double start, bool back
                 return a.first < b.first;
             }
     );
+
+    // Bin pulses by runs.
     for (int i=0; i<pulses.size(); ++i) {
         for (int j=0; j<runs.size(); ++j) {
-            Nexus *runNXS = new Nexus(runs[j]);
+            Nexus *runNXS = new Nexus(runs[j]); // Heap allocation again.
             runNXS->loadBasicData();
+            // Pulse start is between the start and end of the run.
             if ((pulses[i].first>=runNXS->startSinceEpoch) && (pulses[i].second<=runNXS->endSinceEpoch)) {
+                // Add the pulse, making it relative to the start of that run.
                 runPulses[runs[j]].push_back(std::make_pair(pulses[i].first-runNXS->startSinceEpoch,pulses[i].second-runNXS->startSinceEpoch));
                 delete runNXS;
                 break;
             }
+            delete runNXS;
         }
     }
     return true;
