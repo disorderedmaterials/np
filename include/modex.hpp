20439ce81f7b070679ed9b0b3aab3c068649a129
Jared Swift <jared.swift@stfc.ac.uk>
2022-04-12 16:55:01 +0100

    feat: remove debug stuff and more control over extrapolation.

--- include/modex.hpp
+++ include/modex.hpp
@@ -2,6 +2,7 @@
 #define MODEX_H
 
 #include "nexus.hpp"
+#include "config.hpp"
 #include <vector>
 
 class ModEx {
@@ -13,13 +14,15 @@ class ModEx {
         std::vector<std::string> runs;
 
         ModEx(std::string input_, std::string purge_, std::string out_, std::vector<std::string> runs_) : input(input_), purge(purge_), out(out_), runs(runs_) {}
+        ModEx(Config cfg);
         ModEx() = default;
         
         bool run();
         bool run(std::string run, std::vector<std::pair<double, double>> pulses);
         bool run(std::string run, std::pair<double, double> pulses);
-        bool run(std::map<std::string, std::vector<std::pair<double, double>>> &runPulses);
-        bool extrapolatePulseTimes(std::string start_run, double start, bool backwards, bool forwards, double step, double duration, std::map<std::string, std::vector<std::pair<double, double>>> &pulses);
+        bool run(std::map<std::string, std::vector<std::pair<double, double>>> &runPulses, std::string pulseLabel);
+        bool extrapolatePulseTimes(std::string start_run, double start, bool backwards, bool forwards, double periodDuration, double periodOffset, double duration, std::vector<std::pair<double, double>> &pulses);
+        bool binPulsesToRuns(std::vector<std::pair<double, double>> &pulses, std::map<std::string, std::vector<std::pair<double, double>>> &runPulses);
 };
 
 #endif
--- src/modex.cpp
+++ src/modex.cpp
@@ -3,26 +3,20 @@
 #include <algorithm>
 #include <iostream>
 
-std::string extractRunName(std::string &path, std::string &run) {
-    std::size_t preext = path.rfind('.');
-    std::size_t leaf = path.rfind('/');
-
-    return path.substr(leaf+1, preext);
+ModEx::ModEx(Config cfg) {
+    input = cfg.gudrunInputFile;
+    purge = cfg.purgeInputFile;
+    out = cfg.outputDir;
+    runs = cfg.runs;
 }
 
 bool ModEx::run() {
-    // for (auto const run: runs) {
-    //     Nexus nxs = Nexus(run, run.substr(run.find_last_of("/\\") + 1));
-    // }
     return true;
 }
 
-bool ModEx::run(std::map<std::string, std::vector<std::pair<double, double>>> &runPulses) {
-    std::cout << "ModEx begin." << std::endl;
+bool ModEx::run(std::map<std::string, std::vector<std::pair<double, double>>> &runPulses, std::string pulseLabel) {
     // Perform a purge.
-    std::cout << "Performing a singular purge.." << std::endl;
-    system(std::string("./purge_det " + purge).c_str());
-    std::cout << "Purge complete." << std::endl;
+    system(std::string("./purge_det " + purge + "> /dev/null").c_str());
     // Iterate through [run, pulse] pairs.
 
     int n=0;
@@ -30,20 +24,16 @@ bool ModEx::run(std::map<std::string, std::vector<std::pair<double, double>>> &r
     for (const auto runPair : runPulses)
         n+=runPair.second.size();
 
-    std::cout << "There are " << n << "pulses across " << runPulses.size() << " to run on" << std::endl;
-
     for (const auto runPair : runPulses) {
         std::string run = runPair.first;
         std::vector<std::pair<double, double>> pulses = runPair.second;
 
         // Iterate through pulses.
         for (auto p : pulses) {
-            std::cout << "Progress: " << (m/n)*100 << "%" << std::endl;
             // Output path = test/{runName}.nxs
             std::size_t sep = run.rfind('/');
             std::size_t dot = run.rfind('.');
             std::string baseName = run.substr(sep+1, dot-sep-1);
-            std::cout << baseName << std::endl;
             // Allocate a new Nexus objecs on the heap.
             Nexus *nxs = new Nexus(run, "test/" + baseName + ".nxs");
             // Load basic data.
@@ -55,11 +45,10 @@ bool ModEx::run(std::map<std::string, std::vector<std::pair<double, double>>> &r
             // Write the histogram.
             nxs->writeCountsHistogram();
             // Call gudrun_dcs.
-            system(std::string("mkdir modex_intermediate && chdir modex_intermediate && ../gudrun_dcs ../" + input).c_str());
+            system(std::string("mkdir modex_intermediate && chdir modex_intermediate && ../gudrun_dcs ../" + input + " > /dev/null").c_str());
             // Move the mint01 file to the output directory.
-            std::string output = out + "/" + baseName + ".mint01";
+            std::string output = out + "/" + std::to_string(p.first) + "-" + pulseLabel + ".mint01";
             std::string target = "modex_intermediate/" + baseName + ".mint01";
-            std::cout << target << "----> " << output << std::endl;
             system(std::string("mv " + target + " " + output).c_str());
             system("rm -rf modex_intermediate");
             delete nxs;
@@ -73,9 +62,7 @@ bool ModEx::run(std::string run, std::vector<std::pair<double, double>> pulses) 
     return true;
 }
 
-bool ModEx::extrapolatePulseTimes(std::string start_run, double start, bool backwards, bool forwards, double step, double duration, std::map<std::string, std::vector<std::pair<double, double>>> &runPulses) {
-
-    std::vector<std::pair<double, double>> pulses;
+bool ModEx::extrapolatePulseTimes(std::string start_run, double start, bool backwards, bool forwards, double periodDuration, double periodOffset, double duration, std::vector<std::pair<double, double>> &pulses) {
 
     // Assume runs are ordered.
     const std::string firstRun = runs[0];
@@ -92,32 +79,26 @@ bool ModEx::extrapolatePulseTimes(std::string start_run, double start, bool back
     // Determine start, end and first pulse times, since unix epoch.
     const int expStart = firstRunNXS.startSinceEpoch;
     const int expEnd = lastRunNXS.endSinceEpoch;
-    double startPulse = startRunNXS.startSinceEpoch + start;
+    double startPulse = startRunNXS.startSinceEpoch + start + periodOffset;
     double pulse = 0;
-    pulses.push_back(std::make_pair(startPulse, startPulse+step));
-
-    std::cout << "Performing extrapolation.." << std::endl;
+    pulses.push_back(std::make_pair(startPulse, startPulse+duration));
 
     // Extrapolate backwardsa.
     if (backwards) {
-        std::cout << "Extrapolating backwards.." << std::endl;
-        pulse = startPulse - step;
+        pulse = startPulse - periodDuration;
         while (pulse > expStart) {
             pulses.push_back(std::make_pair(pulse, pulse+duration));
-            pulse-=step;
+            pulse-=periodDuration;
         }
-        std::cout  << "Finished extrapolating backwards.." << std::endl;
     }
 
     // Extrapolate forwards.
     if (forwards) {
-        std::cout << "Extrapolating forwards" << std::endl;
-        pulse = startPulse + step;
+        pulse = startPulse + periodDuration;
         while (pulse < expEnd) {
             pulses.push_back(std::make_pair(pulse, pulse+duration));
-            pulse+=step;
+            pulse+=periodDuration;
         }
-        std::cout << "FInished extrapolating forwards.." << std::endl;
     }
 
     // Sort pulses by their start time.
@@ -130,11 +111,14 @@ bool ModEx::extrapolatePulseTimes(std::string start_run, double start, bool back
                 return a.first < b.first;
             }
     );
+    return true;
+}
 
-    // Bin pulses by runs.
+bool ModEx::binPulsesToRuns(std::vector<std::pair<double, double>> &pulses, std::map<std::string, std::vector<std::pair<double, double>>> &runPulses) {
+
     for (int i=0; i<pulses.size(); ++i) {
         for (int j=0; j<runs.size(); ++j) {
-            Nexus *runNXS = new Nexus(runs[j]); // Heap allocation again.
+            Nexus *runNXS = new Nexus(runs[j]); // Heap allocation.
             runNXS->loadBasicData();
             // Pulse start is between the start and end of the run.
             if ((pulses[i].first>=runNXS->startSinceEpoch) && (pulses[i].second<=runNXS->endSinceEpoch)) {
@@ -146,6 +130,5 @@ bool ModEx::extrapolatePulseTimes(std::string start_run, double start, bool back
             delete runNXS;
         }
     }
-    std::cout << "Extrapolation and run binning finished." << std::endl;
     return true;
 }
