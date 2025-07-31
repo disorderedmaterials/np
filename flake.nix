b3e32432f4569379990fef07e87c03b854db6e1d
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2025-07-31 09:28:03 +0100

    Update nix flake.

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
-    future.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     outdated.url = "github:NixOS/nixpkgs/nixos-21.05";
+    future.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
   outputs =
     { self, nixpkgs, future, outdated, flake-utils, bundlers }:
